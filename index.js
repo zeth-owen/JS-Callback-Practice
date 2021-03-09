77ae9286094e5f7c37655a9fce9e527772381c7d
Joshua Miles <joshua.m@hackerusa.com>
2021-03-09 13:26:47 -0600

    -

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character.gif')
-move(character).withArrowKeys(100, 250)
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
+    }
+    if(direction === 'west'){
+        character.src = `assets/green-character/west.gif`
+    }
+    if(direction === 'north'){
+        character.src = `assets/green-character/north.gif`
+    }
+    if(direction === 'east'){
+        character.src = `assets/green-character/east.gif`
+    }
+    if(direction === 'south'){
+        character.src = `assets/green-character/south.gif`
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
 
--- move.js
+++ move.js
@@ -6,7 +6,7 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
-    function moveWithArrowKeys(left, bottom){
+    function moveWithArrowKeys(left, bottom, callback){
         let direction = null;
         let x = left;
         let y = bottom;
@@ -48,10 +48,12 @@ function move(element) {
             if(e.key === 'ArrowDown'){
                 direction = 'south'
             }
+            callback(direction)
         })
         
         document.addEventListener('keyup', function(e){
             direction = null
+            callback(direction)
         })
     }
 

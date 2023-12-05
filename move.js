2c63a95ff7692e71e81de0cd35659ba0e8938205
Zeth Thomas <zeththomas89@gmail.com>
2023-12-05 11:41:24 -0500

    Character_moves

--- move.js
+++ move.js
@@ -1,3 +1,4 @@
+
 function move(element) {
     element.style.position = 'fixed'
 
@@ -5,8 +6,60 @@ function move(element) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
+    
+    function moveWithArrowKeys(left, bottom){
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+    
+    function moveCharacter(){ 
+        if(direction === 'west'){
+            x = x - 1
+        }
+        if(direction === 'north'){
+            y = y + 1
+        }
+        if(direction === 'east'){
+            x = x + 1
+        }
+        if(direction === 'south'){
+            y = y - 1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+    
+    setInterval(moveCharacter, 1)
+  
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+    
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+    })
+    
+    document.addEventListener('keyup', function(e){
+        direction = null
+    })
+}
 
     return {
-        to: moveToCoordinates
-    }
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
 }
+
+
